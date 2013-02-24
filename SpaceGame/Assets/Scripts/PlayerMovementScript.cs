abb4e34b398c6d39aeaf49f4084da8b129ad72cd
Matt Feldman <decaprime@gmail.com>
2013-02-23 16:33:32 -0800

    - Moved player into prefab - Reorganized into folders - Added basic player script for movement

--- SpaceGame/Assets/Scripts/PlayerMovementScript.cs
+++ SpaceGame/Assets/Scripts/PlayerMovementScript.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
 using System.Collections;
 
-public class PlayerMovementScript : MonoBehaviour {
+public class PlayerMovementScript : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody.AddForce(Vector3.left * Input.GetAxis("Horizontal") * -10);
+    }
 }
